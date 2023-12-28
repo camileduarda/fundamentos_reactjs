7544ccb9516c2edd740451f5ccfb686d546fde0f
Camile Paulista <camile.paulista@SKN.LOCAL>
2023-12-28 16:34:15 -0300

    subindo projeto

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react' 6.9k (gzipeed: 2.7k)
+import { ReactDOM } from 'react-dom/client' 513 (gzipped: 320)
+import {App} from './App'
+
+ReactDOM.creatRoot(document.getElementById('root)')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
